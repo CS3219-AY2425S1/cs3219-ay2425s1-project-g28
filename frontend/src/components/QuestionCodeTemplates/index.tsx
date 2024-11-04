5bdbf9aa107271474ac73c50c99bed661dca9e4d
Nicole Lim <limxinyi.nicole@gmail.com>
2024-11-04 16:23:55 +0800

    Fix linting issue

--- frontend/src/components/QuestionCodeTemplates/index.tsx
+++ frontend/src/components/QuestionCodeTemplates/index.tsx
@@ -46,6 +46,7 @@ const QuestionCodeTemplates: React.FC<QuestionCodeTemplatesProps> = ({
     }));
   };
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const handleTabKeys = (event: any) => {
     const { value } = event.target;
 
