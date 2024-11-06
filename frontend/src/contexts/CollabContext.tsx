798819c542bcb8e20e8d5fc08015c5b890edc871
Guan Quan <76832850+guanquann@users.noreply.github.com>
2024-11-06 11:26:24 +0800

    Update code execution

--- frontend/src/contexts/CollabContext.tsx
+++ frontend/src/contexts/CollabContext.tsx
@@ -20,7 +20,7 @@ import { CommunicationEvents } from "../components/Chat";
 import { communicationSocket } from "../utils/communicationSocket";
 import useAppNavigate from "../components/UseAppNavigate";
 
-type CompilerResult = {
+export type CompilerResult = {
   status: string;
   exception: string | null;
   stdout: string;
@@ -30,6 +30,7 @@ type CompilerResult = {
   stout: string;
   actualResult: string;
   expectedResult: string;
+  isMatch: boolean;
 };
 
 type CollabContextType = {
@@ -40,6 +41,7 @@ type CollabContextType = {
   checkPartnerStatus: () => void;
   setCode: React.Dispatch<React.SetStateAction<string>>;
   compilerResult: CompilerResult[];
+  setCompilerResult: React.Dispatch<React.SetStateAction<CompilerResult[]>>;
   isEndSessionModalOpen: boolean;
 };
 
@@ -79,12 +81,12 @@ const CollabProvider: React.FC<{ children?: React.ReactNode }> = (props) => {
     try {
       const res = await codeExecutionClient.post("/", {
         questionId,
-        code,
+        // Replace tabs with 4 spaces to prevent formatting issues
+        code: code.replace(/\t/g, " ".repeat(4)),
         language: matchCriteria?.language.toLowerCase(),
       });
-
-      console.log(res.data.data);
-      setCompilerResult(res.data.data);
+      console.log([...res.data.data]);
+      setCompilerResult([...res.data.data]);
 
       let isMatch = true;
       for (let i = 0; i < res.data.data.length; i++) {
@@ -166,6 +168,7 @@ const CollabProvider: React.FC<{ children?: React.ReactNode }> = (props) => {
         checkPartnerStatus,
         setCode,
         compilerResult,
+        setCompilerResult,
         isEndSessionModalOpen,
       }}
     >
