be86ef94f9832d8c100dbbfa467703837c4904cb
jolynloh <lohyuanenjolyn@gmail.com>
2024-11-08 20:58:21 +0800

    Config and edits

--- backend/question-service/src/server.ts
+++ backend/question-service/src/server.ts
@@ -1,5 +1,6 @@
 import app from "./app.ts";
 import connectDB from "./config/db.ts";
+import { seedQuestions } from "./scripts/seed.ts";
 
 const PORT = process.env.SERVICE_PORT || 3000;
 
@@ -7,12 +8,15 @@ if (process.env.NODE_ENV !== "test") {
   connectDB()
     .then(() => {
       console.log("MongoDB Connected!");
+      // seedQuestions();
 
-      app.listen(PORT, () => {
+      const server = app.listen(PORT, () => {
         console.log(
           `Question service server listening on http://localhost:${PORT}`,
         );
       });
+
+      server.keepAliveTimeout = 70 * 1000; // set timeout value to > load balancer idle timeout (60s)
     })
     .catch((err) => {
       console.error("Failed to connect to DB");
