1c043dee4974af44f8b86ba33e6ff21f2fc68b13
jolynloh <lohyuanenjolyn@gmail.com>
2024-11-08 21:06:16 +0800

    Remove console log...

--- frontend/src/utils/matchSocket.ts
+++ frontend/src/utils/matchSocket.ts
@@ -3,8 +3,6 @@ import { io } from "socket.io-client";
 const MATCH_SOCKET_URL =
   import.meta.env.VITE_MATCH_SERVICE_URL ?? "http://localhost:3002";
 
-console.log(import.meta.env.VITE_MATCH_SERVICE_URL);
-
 export const matchSocket = io(MATCH_SOCKET_URL, {
   reconnectionAttempts: 3,
   autoConnect: false,
