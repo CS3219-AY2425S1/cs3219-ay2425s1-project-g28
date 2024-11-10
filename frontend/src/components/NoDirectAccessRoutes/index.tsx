db722a0f7e7be1da7b3d16a2e375f9fb890f6af7
Tin Ruiqi <tinruiqi2002@gmail.com>
2024-11-10 16:39:38 +0800

    Define location state type

--- frontend/src/components/NoDirectAccessRoutes/index.tsx
+++ frontend/src/components/NoDirectAccessRoutes/index.tsx
@@ -1,9 +1,10 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { LocationState } from "../../hooks/useAppNavigate";
 
 const NoDirectAccessRoutes: React.FC = () => {
   const location = useLocation();
 
-  if (location.state?.from !== "app-navigation") {
+  if ((location.state as LocationState)?.from !== "app-navigation") {
     return <Navigate to="/home" replace />;
   }
 
